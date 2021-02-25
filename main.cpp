d820a07547d61bf850cebd5f71b2bc43e6dab207
SHLIU1 <SHLIU1@OANBE02333.nuvoton.com>
2021-02-25 11:24:43 +0800

    Support the both V5.X and V6.X for mbed-os

--- main.cpp
+++ main.cpp
@@ -8,11 +8,18 @@ int main() {
     
     int i=0;
     printf("...DCserv Start...\n\r");
+#ifdef MBED_MAJOR_VERSION
+    printf("Mbed OS version %d.%d.%d\r\n\n", MBED_MAJOR_VERSION, MBED_MINOR_VERSION, MBED_PATCH_VERSION);
+#endif
     pwm1.period_us(20000);      // set PWM period to 20ms (50Hz)
         
     for (i=500; i<=2500; i=i+200) { // from 0.5ms to 2.5ms
         pwm1.pulsewidth_us(i);      // set PWM pulse width to rotate motor
+#if MBED_MAJOR_VERSION >= 6
+        ThisThread::sleep_for(1000);
+#else
         Thread::wait(1000);         // delay
+#endif
         printf("DCservo pulse width = %d\n\r", i);
     } 
     printf("...DCserv End.....\n\r");                             
